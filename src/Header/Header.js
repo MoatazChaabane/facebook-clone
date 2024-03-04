7fefb3bb41662082eef1ce09f834e808ba83cf42
Moataz Chaabane <chaabanemoataz@outlook.fr>
2024-03-04 18:47:41 +0100

    first

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div className='header'>Header
+    <div className='header_left'></div>
+
+    <div className='header_right'></div>
+
+    <div className='header_middle'></div>
+
+    
+    </div>
+  
+    )
+}
+
+export default Header
